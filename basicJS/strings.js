32dea10c41e71dd726383d878142cf020d67c854
Chaim <c0507634153@gmail.com>
2026-06-28 09:00:08 +0300

    add strings

---
+++ basicJS/strings.js
@@ -0,0 +1,104 @@
+
+// 1
+// const str = " hello world "
+// console.log(str.trim());
+
+
+// 2
+// const email = "user@gmail.com";
+// console.log(email.includes("@"));
+
+
+// 3
+// const str = "javascript";
+// console.log(str.toUpperCase());
+
+
+// 4
+// const str = "HELLO WORLD";
+// console.log(str.toLowerCase());
+
+
+// 5
+// const str = "HELLO WORLD";
+// console.log(str.length);
+
+// 6
+// const str = "HELLO WORLD";
+// console.log(str.slice(0, 4));
+
+// 7
+// const str = "HELLO WORLD";
+// console.log(str.slice(0, 6));
+
+// 8
+// const str = "HELLO WORLD";
+// console.log(str.startsWith("HEL"));
+
+// 9
+// const str = "HELLO WORLD";
+// console.log(str.endsWith("RLD"));
+
+
+// 10
+// const str = "HELLO WORLD";
+// console.log(str.replace("O", "H"));
+
+// 11
+// const str = "HELLO WORLD";
+// console.log(str.replaceAll("O", "H"));
+
+// 12
+// const str = "k l j";
+// console.log(str.split(" "));
+
+// 13
+// const str = "k l j";
+// console.log(str.split(" ").slice(0, 2));
+
+// 14
+// const str = "banana";
+// console.log(str.indexOf("a"));
+
+// 15
+// const str = "banana";
+// console.log(str.lastIndexOf("na"));
+
+// 16
+// const str = "7";
+// console.log(str.padStart(2, "0"));
+
+// 17
+// const str = "hi";
+// console.log(str.padEnd(5, "*"));
+
+// 18
+// const str = "jhg ";
+// console.log(str.repeat(5));
+
+// 19
+// const str = "asdfghjkl";
+// console.log(str.charAt(20));
+
+// 20
+// const str = "asdfghjkl";
+// console.log(str.at(-2));
+
+// 21
+// console.log("hi".concat(" ", "Chaim"," ", "dfghj"));
+
+// 22
+// const str = "asdfghjkl";
+// console.log(str.substring(2, 6));
+
+// 23
+// const str = "AdminPanel";
+// console.log(str.toLowerCase().includes("admin".toLowerCase()));
+
+// 24
+// const str = " dfghj   ";
+// console.log(str.trimStart().length);
+
+// 25
+// const str = " dfghj   ";
+// console.log(str.trimEnd().length);
